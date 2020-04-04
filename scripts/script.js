27decc9f524dadd20ed0561eebbf8fadafbcb945
Marco <arboleda.marco@gmail.com>
2020-04-04 17:40:50 -0400

    Make the grid have 16 rows and columns

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,15 @@
 const container = document.querySelector("#container");
 
-for (i = 0; i < 16; i++) {
+for (i = 0; i < (16 * 16); i++) {
     const newCell = document.createElement("div");
     newCell.setAttribute("class", "cell");
     container.appendChild(newCell);
 }
+
+let numColumnString = "";
+for (i = 0; i < 16; i++) {
+    numColumnString += "auto ";
+}
+
+container.style["grid-template-columns"] = numColumnString;
+container.style["grid-template-rows"] = numColumnString; //num of rows and num of columns is the same
