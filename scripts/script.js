45166d60631b36c456982ed97baf5bda81e6c65b
Marco Arboleda <arboleda.marco@gmail.com>
2020-06-19 17:45:11 -0400

    Fix remaining issues reported via ESLint

--- scripts/script.js
+++ scripts/script.js
@@ -4,12 +4,8 @@ const normalModeButton = document.querySelector('#normal-button');
 const discoModeButton = document.querySelector('#disco-button');
 const gradualModeButton = document.querySelector('#gradual-button');
 
-generateNewCells(16);
-container.style['grid-template-columns'] = generateGridTemplateString(16);
-container.style['grid-template-rows'] = generateGridTemplateString(16);
-
 function generateNewCells(numOfRows) {
-  for (i = 0; i < (numOfRows * numOfRows); i++) {
+  for (let i = 0; i < (numOfRows * numOfRows); i++) {
     const newCell = document.createElement('div');
     newCell.setAttribute('class', 'cell');
     container.appendChild(newCell);
@@ -17,7 +13,7 @@ function generateNewCells(numOfRows) {
 
   const cells = document.querySelectorAll('.cell');
   cells.forEach((cell) => {
-    cell.onmouseover = function (e) {
+    cell.onmouseover = (e) => {
       e.target.style['background-color'] = 'rgba(0 ,0, 0, 1)';
     };
   });
@@ -25,16 +21,20 @@ function generateNewCells(numOfRows) {
 
 function generateGridTemplateString(numOfColumns) {
   let numOfColumnsAndRowsString = '';
-  for (i = 0; i < numOfColumns; i++) {
+  for (let i = 0; i < numOfColumns; i++) {
     numOfColumnsAndRowsString += 'auto ';
   }
   return numOfColumnsAndRowsString;
 }
 
+generateNewCells(16);
+container.style['grid-template-columns'] = generateGridTemplateString(16);
+container.style['grid-template-rows'] = generateGridTemplateString(16);
+
 function generateNewGrid() {
   document.querySelectorAll('.cell').forEach((cell) => cell.remove());
 
-  gridSize = prompt('How many rows/columns would you like your grid to have?');
+  const gridSize = prompt('How many rows/columns would you like your grid to have?');
 
   container.style['grid-template-columns'] = generateGridTemplateString(gridSize);
   container.style['grid-template-rows'] = generateGridTemplateString(gridSize);
@@ -42,24 +42,24 @@ function generateNewGrid() {
   generateNewCells(gridSize);
 }
 
-normalModeButton.onclick = function () {
+normalModeButton.onclick = () => {
   const cells = document.querySelectorAll('.cell');
   mode.textContent = 'Normal';
   cells.forEach((cell) => {
-    cell.onmouseover = function (e) {
+    cell.onmouseover = (e) => {
       e.target.style['background-color'] = 'rgba(0, 0, 0, 1)';
     };
   });
 };
 
-discoModeButton.onclick = function () {
+discoModeButton.onclick = () => {
   const cells = document.querySelectorAll('.cell');
   let rValue;
   let gValue;
   let bValue;
   mode.textContent = 'Disco';
   cells.forEach((cell) => {
-    cell.onmouseover = function (e) {
+    cell.onmouseover = (e) => {
       rValue = Math.round((Math.random() * 255));
       gValue = Math.round((Math.random() * 255));
       bValue = Math.round((Math.random() * 255));
@@ -68,17 +68,17 @@ discoModeButton.onclick = function () {
   });
 };
 
-gradualModeButton.onclick = function () {
+gradualModeButton.onclick = () => {
   const cells = document.querySelectorAll('.cell');
   mode.textContent = 'Gradual';
   cells.forEach((cell) => {
-    cell.onmouseover = function (e) {
-      if (e.target.style['background-color'] != ''
-                && e.target.style['background-color'].substr(0, 12) == 'rgba(0, 0, 0') {
+    cell.onmouseover = (e) => {
+      if (e.target.style['background-color'] !== ''
+                && e.target.style['background-color'].substr(0, 12) === 'rgba(0, 0, 0') {
         const alphaVal = Number(e.target.style['background-color'].substr(-4, 3));
         e.target.style['background-color'] = `rgba(0, 0, 0, ${alphaVal + 0.1})`;
-      } else if (e.target.style['background-color'] != ''
-                       && e.target.style['background-color'].substr(0, 11) == 'rgb(0, 0, 0') {
+      } else if (e.target.style['background-color'] !== ''
+                       && e.target.style['background-color'].substr(0, 11) === 'rgb(0, 0, 0') {
         // do nothing
       } else { // blank cell
         e.target.style['background-color'] = 'rgba(0, 0, 0, 0.1)';
